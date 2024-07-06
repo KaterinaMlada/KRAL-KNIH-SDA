16d6bedf6da4d51898f8d1beac11e8d7b66c1cff
Stepan Kubicek <kubiceks@seznam.cz>
2024-07-06 10:16:44 +0200

    Stepan 0607-2 - admin-customer-list

--- core/admin.py
+++ core/admin.py
@@ -10,13 +10,18 @@ class BookAdmin(ModelAdmin):
     list_per_page = 15
 
 
+@admin.register(Customer)
+class CustomerAdmin(ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+
+
 admin.site.register(Author)
 
 admin.site.register(Promotion)
 
 admin.site.register(Role)
-
-admin.site.register(Customer)
 
 admin.site.register(Address)
 
