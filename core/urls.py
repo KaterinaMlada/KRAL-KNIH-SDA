50827d5c45746069f3ede49937bdaa259ffb51b5
KaterinaMlada <Mlada.katka2@seznam.cz>
2024-07-17 13:29:11 +0200

    17.07. Katka Mladá Úprava aby byla vidět cena, taky přidání výběrů z kategorií do detailu knih

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,25 @@
 from django.urls import path
 from . import views
+from .views import cart_count
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.BooksView.as_view(), name="books"),
     path('books/<pk>/', views.BookDetailView.as_view(), name="book_detail"),
+    
     path('category/<int:category_id>/', views.BooksByCategoryView.as_view(), name='books_by_category'), 
 
     path('cart/', views.cart, name="cart"),
     path('add-to-cart/<int:book_id>/', views.add_to_cart, name="add_to_cart"),
+    path('add-to-cart-detail/<int:book_id>/', views.add_to_cart_detail, name="add_to_cart_detail"),
     path('update-cart-item/<int:book_id>/<str:action>/', views.update_cart_item, name="update_cart_item"),
     path('remove-from-cart/<int:book_id>/', views.remove_from_cart, name="remove_from_cart"),
-
+    path('cart/count/', cart_count, name='cart_count'),
+    
     path('checkout/', views.checkout, name="checkout"),
 
     path('about/', views.show_about, name='about'),
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -3,6 +3,9 @@ from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from core.models import Book, Cart, CartItem, Category
+from django.http import JsonResponse
+
+
 
 class BooksView(ListView):
     template_name = 'books.html'
@@ -16,6 +19,12 @@ class BooksView(ListView):
 class BookDetailView(DetailView):
     template_name = 'book_detail.html'
     model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_book = self.get_object()
+        context['related_books'] = Book.objects.filter(category=current_book.category).exclude(pk=current_book.pk)[:3]
+        return context
 
 class BooksByCategoryView(ListView):
     template_name = 'books.html'
@@ -53,6 +62,8 @@ def add_to_cart(request, book_id):
         cart = Cart.objects.create()
         request.session['cart_id'] = str(cart.id)
 
+
+
     
     cart_item, item_created = CartItem.objects.get_or_create(cart=cart, book=book)
 
@@ -61,7 +72,30 @@ def add_to_cart(request, book_id):
         cart_item.quantity += 1
         cart_item.save()
 
-    return redirect('core:cart')
+    return redirect('core:books')
+
+
+def add_to_cart_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+ 
+    cart_id = request.session.get('cart_id')
+    if cart_id:
+        cart = get_object_or_404(Cart, id=cart_id)
+    else:
+        cart = Cart.objects.create()
+        request.session['cart_id'] = str(cart.id)
+
+
+    cart_item, item_created = CartItem.objects.get_or_create(cart=cart, book=book)
+    
+   
+    if not item_created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+
+    return redirect('core:book_detail', pk=book.pk)
+
 
 def cart(request):
     cart_id = request.session.get('cart_id')
@@ -107,3 +141,11 @@ def remove_from_cart(request, book_id):
 
 
 
+def cart_count(request):
+    cart_id = request.session.get('cart_id')
+    if cart_id:
+        cart = get_object_or_404(Cart, id=cart_id)
+        cart_count = cart.cartitem_set.count()
+    else:
+        cart_count = 0
+    return JsonResponse({'count': cart_count})
