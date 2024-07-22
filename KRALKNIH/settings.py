2684b8886bdf4201d4a35674bd108bb8192fc943
Stepan Kubicek <kubiceks@seznam.cz>
2024-07-22 09:59:15 +0200

    Stepan 2207 - prihlaseni/odhlaseni

--- KRALKNIH/settings.py
+++ KRALKNIH/settings.py
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-
+LOGOUT_REDIRECT_URL = '/'
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            
+            user = form.get_user()
+            login(request, user)
             return redirect('core:books')  
     else:
         form = AuthenticationForm()
@@ -29,3 +30,8 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('core:books')
